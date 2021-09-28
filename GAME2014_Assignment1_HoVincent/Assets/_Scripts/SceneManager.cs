6499e13a68a90a32a150717bba6cf976404a5d3a
Vincent329 <vincee.ho@gmail.com>
2021-09-27 20:25:27 -0400

    [ADD] TextMeshPro Essentials

---
+++ GAME2014_Assignment1_HoVincent/Assets/_Scripts/SceneManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    enum Menus
+    {
+        MAINMENU = 0,
+        INSTRUCTIONS = 1,
+        GAME = 2,
+        GAMEOVER = 3
+    }
+}
